889ad375a086187175a9d18bbbbd52f5e3680f34
Brock Ellis <brock@sharproot.com>
2014-11-14 23:57:41 -0600

    Project is in a working state with hard coded information. OAuth is next!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 App = Ember.Application.create();
 
+App.Student = Ember.Object.extend({
+    FirstName: null,
+    LastName: null,
+    ID: null,
+
+    FullName: function(){
+        return this.get('FirstName') + ' ' + this.get('LastName');
+    }.property('FirstName', 'LastName'),
+
+    PhotoURL: function(){
+        //var url = "https://app.sycamoreeducation.com/api/v1/Students/" + this.get('ID')
+
+        return "https://app.sycamoreeducation.com/School/1002/Student/" + this.get("ID") + "/";
+    }
+});
+
 App.Router.map(function() {
   this.resource("index", { path: "/" }, function(){
     this.resource("student", { path: ":firstname" }, function(){
@@ -9,41 +25,110 @@ App.Router.map(function() {
     });
   });
   this.resource("about");
+  this.resource("login");
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     //return students;
-    var url = "https://app.sycamoreeducation.com/Family/647150/Students";
+    var url = "https://app.sycamoreeducation.com/api/v1/Family/647150/Students";
     return Ember.$.getJSON(url);
   }
 });
 
+//redirect uri's cannot have hashes in them, which caused issues
+/*App.LoginRoute = Ember.Route.extend({
+  beforeModel: function(){
+    $.getScript("js/oauth.js");
+  }
+});*/
+
 App.StudentRoute = Ember.Route.extend({
-  model: function(params){
-    return students.findBy('firstname', params.firstname);
-  },
-  afterModel: function(){
+  //model: function(params){
+  //  return students.findBy('FirstName', params.firstname);
+  //},
+  beforeModel: function(){
     this.transitionTo('grades');
   }
 });
 
-var students = [
-  {
-    id: '1',
-    firstname: "Ansleigh",
-    lastname: "Ellis",
-    age: '2',
-    grade: "K"
+App.GradesRoute = Ember.Route.extend({
+    beforeModel: function() {
+      // Assume the 'loading' class displays an overlay with a loading animation
+      Ember.$('.spinner').css("display", "block");
+    }, 
+    model: function(){
+      var url = "https://app.sycamoreeducation.com/api/v1/Student/614085/Grades";
+      var grades = [];
+      //return Ember.$.getJSON(url);
+      return Ember.$.getJSON(url).then(function(data){
+        $.each(data, function(index, obj){
+          var grade = {};
+          $.each(obj, function(key, value){
+            grade[key.toLowerCase()] = value;
+          });
+          grades.push(grade);
+        });
+        //console.log(grades);
+        return grades;
+      });
+    },
+    afterModel: function() {
+      Ember.$('.spinner').css("display", "none");
+    }
+});
+
+App.HomeworkRoute = Ember.Route.extend({
+    beforeModel: function() {
+      // Assume the 'loading' class displays an overlay with a loading animation
+      Ember.$('.spinner').css("display", "block");
+    }, 
+    model: function(){  
+      var url = "https://app.sycamoreeducation.com/api/v1/Student/614085/Homework";
+      var homework = [];
+      //return Ember.$.getJSON(url);
+      return Ember.$.getJSON(url).then(function(data){
+        $.each(data, function(index, obj){
+          var assignment = {};
+          $.each(obj, function(key, value){
+            assignment[key.toLowerCase()] = value;
+          });
+          homework.push(assignment);
+        });
+        //console.log(homework);
+        return homework;
+    })
   },
-  {
-    id: '2',
-    firstname: "Mason",
-    lastname: "Ellis",
-    age: '0',
-    grade: '0'
+  afterModel: function() {
+    Ember.$('.spinner').css("display", "none");
   }
-];
+})
+
+App.MissingRoute = Ember.Route.extend({
+    beforeModel: function() {
+      // Assume the 'loading' class displays an overlay with a loading animation
+      Ember.$('.spinner').css("display", "block");
+    }, 
+    model: function(){
+      var url = "https://app.sycamoreeducation.com/api/v1/Student/614085/Missing";
+      var missing = [];
+      //return Ember.$.getJSON(url);
+      return Ember.$.getJSON(url).then(function(data){
+        $.each(data, function(index, obj){
+          var assignment = {};
+          $.each(obj, function(key, value){
+            assignment[key.toLowerCase()] = value;
+          });
+          missing.push(assignment);
+        });
+        //console.log(grades);
+        return missing;
+      });
+    },
+    afterModel: function() {
+      Ember.$('.spinner').css("display", "none");
+    }
+});
 
 $.ajaxSetup({
    'beforeSend': function(xhr) {           
---
+++ js/oauth.js
@@ -0,0 +1,39 @@
+var client_id = "5466e01ee3fc3"; // Replace text in quotes with your API Key
+var callback_uri = "http://localhost/Frontpage/login.html";
+//var callback_uri = "http://www.google.com";
+
+function parseParams (params) {
+    var sycamoreAuth = Array();
+    var authData = params.split("&",4);
+    for (i=0; i < authData.length; i++) {
+        var paramVal = authData[i].split("=",2);
+        sycamoreAuth.push(paramVal[1]);
+    }
+    
+    return sycamoreAuth;
+}
+
+function setLocalStorage(token){
+    localStorage.setItem("sycamore_auth", token);
+    return;
+}
+
+//#/login will always have 7 characters...ugly, ugly hack
+if (window.location.hash.length == 0) {
+    var path = 'https://app.sycamoreeducation.com/oauth/authorize.php?';
+    var queryParams = ['client_id=' + client_id,
+                       'redirect_uri=' + callback_uri,
+                       'scope=general open individual',
+                       'response_type=token'];
+    var query = queryParams.join('&');
+    var url = path + query;
+    
+    window.open(url,"_self");
+} else {
+    var rawParams = window.location.hash.substring(1);
+    var oAuthData = parseParams(rawParams);
+
+    setLocalStorage(oAuthData[0]);
+  
+    window.open("index.html", "_self");
+}
